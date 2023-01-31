c353b12768c8fe1f5890181db3c95821798797f8
Jeffrey Jing <jjingdn@gmail.com>
2023-01-31 12:23:46 -0800

    removed node_modules

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import { useState, useEffect } from 'react';
+
+import MovieCard from './MovieCard';
+import SearchIcon from './search.svg'
+import './App.css'
+
+
+const API_URL = 'http://www.omdbapi.com?apikey=decc96d7'
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    searchMovies("Star Wars");
+  }, []);
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`)
+    const data = await response.json();
+    setMovies(data.Search);
+  };
+
+
+  return (
+    <div className="app">
+      <h1>Movieland</h1>
+
+      <div className="search">
+        <input
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="Search for Movies"
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => searchMovies(searchTerm)}
+        />
+      </div>
+
+      {movies?.length > 0 ? (
+          <div className="container">
+            {movies.map((movie) => (
+              <MovieCard movie={movie} />
+            ))}
+            </div>
+        ) : (
+          <div className="empty">
+            <h2>No movies found</h2>
+          </div>
+        )}
+    </div>
+  );
+};
+
+export default App;
